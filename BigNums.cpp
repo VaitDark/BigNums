60d1d096a86efac4b85b794820eb24970dce4c90
VaitDark <vait.dark@yandex.ru>
2023-06-22 19:52:52 +0300

    add

--- BigNums.cpp
+++ BigNums.cpp
@@ -13,7 +13,58 @@ num::num(int n){
 num::num(std::string str){
 		for(int i=0; i<size(str); i++){
 			v.push_back(str[i]);
-			std::cout<<v[i];
+			//std::cout<<v[i];
 
 	}
+}
+num operator + (num& num1, num& num2){
+	std::vector<char>::iterator itr1=num1.v.end()-1;
+	std::vector<char>::iterator itr2=num2.v.end()-1;
+	std::vector<char>::iterator itr;
+	std::vector<char>::iterator itrb;
+	std::vector<char>::iterator itro;
+	bool f;
+	if (size(num1.v)<=size(num2.v)){
+		 itr=itr1;
+		 itro=num1.v.begin();
+		 itrb=itr2;
+		 f=true;
+	}else{
+		 itr=itr2;
+		 itro=num2.v.begin();
+		 itrb=itr1;
+		 f=false;
+	}
+
+	int sn=0;
+	for(itr; itr!=itro;--itr){
+		int n1=int(*itr+sn)-48;
+		int n2=int(*itrb)-48;
+		n1+=n2;
+		sn=n1/10;
+		n1=n1%10;
+		*itrb=char(n1+48);
+		itrb=itrb-1;
+	}
+	int n1=int(*itr+sn)-48;
+	int n2=int(*itrb)-48;
+	n1+=n2;
+	sn=n1/10;
+	n1=n1%10;
+	*itrb=char(n1+48);
+	if(sn!=0){
+		num1.v.emplace(itrb,sn);
+	}
+	if(!f){
+		return num1;
+	}else{
+		return num2;
+	}
+
+}
+std::ostream& operator <<(std::ostream& stream, num& num1){
+	for(auto i=num1.v.begin(); i!=num1.v.end(); ++i){
+		stream <<*i;
+	}
+	return stream;
 }
--- include/BigNums.h
+++ include/BigNums.h
@@ -10,5 +10,6 @@ public:
 	std::vector<char> v;
 	num(int n);
 	num(std::string str);
-	
+	friend num operator+(num& num1, num& num2);
+	friend std::ostream& operator <<(std::ostream& stream, num& num1);
 };
